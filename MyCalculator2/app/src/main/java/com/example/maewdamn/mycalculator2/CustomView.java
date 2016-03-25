0dd29e8ea701161af116b10a20edae7b9713d40b
ChakritTora <chakrit.tora@gmail.com>
2016-03-25 12:31:01 +0700

    update android file and homework

---
+++ MyCalculator2/app/src/main/java/com/example/maewdamn/mycalculator2/CustomView.java
@@ -0,0 +1,158 @@
+package com.example.maewdamn.mycalculator2;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.TypedValue;
+import android.view.GestureDetector;
+import android.view.MotionEvent;
+import android.view.View;
+
+/**
+ * Created by Chakrit Tokuhara on 24/3/2559.
+ */
+
+public class CustomView extends View {
+    private boolean isDown = false;
+    private boolean isBlue = false;
+    private GestureDetector gestureDetector;
+
+
+
+    private void initWithAttrs(AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        TypedArray a = getContext().getTheme().obtainStyledAttributes(
+                attrs,
+                R.styleable.CustomView,
+                defStyleAttr, defStyleRes);
+        try {
+            isBlue = a.getBoolean(R.styleable.CustomView_isBlue, false);
+
+        } finally {
+            a.recycle();
+        }
+    }
+
+    public CustomView(Context context) {
+        super(context);
+        init();
+    }
+
+    public CustomView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+        initWithAttrs(attrs, 0, 0);
+    }
+
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+        initWithAttrs(attrs, defStyleAttr, 0);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init();
+        initWithAttrs(attrs, defStyleAttr, defStyleRes);
+    }
+
+
+
+
+
+    private void init() {
+        int a = 1;
+        gestureDetector = new GestureDetector(getContext(), new GestureDetector.OnGestureListener() {
+            @Override
+            public boolean onDown(MotionEvent e) {
+                getParent().requestDisallowInterceptTouchEvent(true);
+                return false;
+            }
+
+            @Override
+            public void onShowPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onSingleTapUp(MotionEvent e) {
+                return false;
+            }
+
+            @Override
+            public boolean onScroll(MotionEvent e1, MotionEvent e2, float distanceX, float distanceY) {
+                return false;
+            }
+
+            @Override
+            public void onLongPress(MotionEvent e) {
+
+            }
+
+            @Override
+            public boolean onFling(MotionEvent e1, MotionEvent e2, float velocityX, float velocityY) {
+                isBlue = !isBlue;
+                invalidate();
+                return false;
+
+            }
+        });
+
+        setClickable(true);
+
+        setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+
+        Paint p = new Paint();
+        if (isBlue) {
+            p.setColor(0xFF0000FF);
+        } else {
+            p.setColor(0xFFFF0000);
+        }
+
+        canvas.drawLine(0, 0, getMeasuredWidth(), getMeasuredHeight(), p);
+
+        if (isDown) {
+            p.setColor(0xFF00Ff00);
+            float px = TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 5, getContext().getResources().getDisplayMetrics());
+            p.setStrokeWidth(px);
+
+            canvas.drawLine(0, getMeasuredHeight(), getMeasuredWidth(), 0, p);
+        }
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                isDown = true;
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_MOVE:
+                break;
+            case MotionEvent.ACTION_UP:
+                isDown = false;
+                invalidate();
+                return true;
+            case MotionEvent.ACTION_CANCEL:
+                isDown = false;
+                invalidate();
+                return true;
+        }
+        return super.onTouchEvent(event);
+    }
+    
+}
